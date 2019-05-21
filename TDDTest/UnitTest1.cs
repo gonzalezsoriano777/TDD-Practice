c9f81b33498089e6723dd8e36449d0e70b6f2cfe
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-05-21 11:31:29 -0500

    test

--- TDDTest/UnitTest1.cs
+++ TDDTest/UnitTest1.cs
@@ -4,11 +4,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace TDDTest
 {
     [TestClass]
-    public class UnitTest1
+    public class RomanNumeralsTesting
     {
         [TestMethod]
-        public void BusinessTestMethod()
+        public void Parse()
         {
+
 
         }
     }
