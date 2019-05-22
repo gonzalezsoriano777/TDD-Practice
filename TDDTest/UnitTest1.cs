4649f200341db11849b73ef9775c79e2e00696de
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-05-22 09:55:46 -0500

    test

--- TDDTest/UnitTest1.cs
+++ TDDTest/UnitTest1.cs
@@ -1,23 +1,31 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+
 namespace TDDTest
 {
     [TestClass]
     public class RomanNumeralsTesting
     {
+
+
+        
         [TestMethod]
         public void Parse()
         {
             Assert.AreEqual(1, Roman.ParseNumeral("I"));
             Assert.AreEqual(5, Roman.ParseNumeral("V"));
             Assert.AreEqual(10, Roman.ParseNumeral("X"));
+            Assert.AreEqual(6, Roman.ParseNumeral("IV"));
         }
     }
 
     
     public class Roman
     {
+        
+        
+
         public static int ParseNumeral(string roman)
         {
 
@@ -26,6 +34,9 @@ namespace TDDTest
 
             if (roman == "X")
                 return 10;
+
+            if (roman == "IV")
+                return 6;
 
             // represented as the first roman numeral
             return 1;
